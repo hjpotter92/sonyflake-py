7bf1fecbf2834a5582b990698da85777822b3249
ZipFile <zipfile.d@protonmail.com>
2024-08-17 15:23:26 +0000

    Expose more in base sonyflake module

--- sonyflake/__init__.py
+++ sonyflake/__init__.py
@@ -1,4 +1,17 @@
 from .about import NAME, VERSION, __version__
-from .sonyflake import SonyFlake
+from .round_robin import RoundRobin
+from .sonyflake import (
+    SONYFLAKE_EPOCH,
+    SonyFlake,
+    lower_16bit_private_ip,
+    random_machine_id,
+    random_machine_ids,
+)
 
-__all__ = ["SonyFlake"]
+__all__ = [
+    "RoundRobin",
+    "SonyFlake",
+    "random_machine_id",
+    "random_machine_ids",
+    "lower_16bit_private_ip",
+]
